86f0117aca9a7e9f01e311d8e25a16a14c62cbbd
lexplex1212 <lupu.alex1212@gmail.com>
2026-05-20 17:19:25 +0200

    Änderungen an index.js

--- js/index.js
+++ js/index.js
@@ -1231,7 +1231,7 @@ function toggleWidget() {
 });
 
 
-// HTTPS-Server starten
+// HTTPS-Server
 const port = process.env.PORT || 3000;
 const httpsOpts = {
     key: fs.readFileSync('./cert/key.pem'),
